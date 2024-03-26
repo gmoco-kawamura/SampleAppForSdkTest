4035735b259b33a7ca087f4d8c0aaf78b34dcfa7
gmoco-kawamura <158865565+gmoco-kawamura@users.noreply.github.com>
2024-03-26 14:58:10 +0900

    add code at settings.gradle.kts

--- android-lib-kawamura0/build.gradle.kts
+++ android-lib-kawamura0/build.gradle.kts
@@ -55,7 +55,7 @@ dependencies {
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
-    implementation("com.github.gmoco-kawamura:SampleAppForSdkTest:0.0.2")
+    implementation("com.github.gmoco-kawamura:SampleAppForSdkTest:0.0.3")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
