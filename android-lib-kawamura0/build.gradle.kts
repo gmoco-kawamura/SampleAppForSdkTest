1b2089d05dab5802fca445b63334971dee747082
gmoco-kawamura <158865565+gmoco-kawamura@users.noreply.github.com>
2024-03-26 16:36:28 +0900

    fix java version at build.gradle.kts

--- android-lib-kawamura0/build.gradle.kts
+++ android-lib-kawamura0/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_1_17
+        targetCompatibility = JavaVersion.VERSION_1_17
     }
     kotlinOptions {
         jvmTarget = "1.17"
@@ -55,7 +55,7 @@ dependencies {
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
-    implementation("com.github.gmoco-kawamura:SampleAppForSdkTest:0.0.7")
+    implementation("com.github.gmoco-kawamura:SampleAppForSdkTest:0.0.8")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
