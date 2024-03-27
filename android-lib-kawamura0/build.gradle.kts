77b69bf0d194bc0320746e56e4e4e6e4a39d34ca
gmoco-kawamura <158865565+gmoco-kawamura@users.noreply.github.com>
2024-03-27 17:19:39 +0900

    add AdvancedWebView.java

--- android-lib-kawamura0/build.gradle.kts
+++ android-lib-kawamura0/build.gradle.kts
@@ -9,7 +9,7 @@ android {
     compileSdk = 34
 
     defaultConfig {
-        minSdk = 24
+        minSdk = 23
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         consumerProguardFiles("consumer-rules.pro")
@@ -39,7 +39,7 @@ afterEvaluate {
             create("release", MavenPublication::class.java) {
                 groupId = "com.github.gmoco-kawamura"
                 artifactId = "SampleAppForSdkTest"
-                version = "0.1.4"
+                version = "0.1.5"
                 artifact("$buildDir/outputs/aar/android-lib-kawamura0-release.aar")
             }
         }
@@ -55,7 +55,6 @@ dependencies {
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
-//    implementation("com.github.gmoco-kawamura:SampleAppForSdkTest:0.1.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
